d4e4d674a1c16b387675e47f72414b1ad14fc299
alifoo <alissonayres.trb@gmail.com>
2025-03-03 17:44:17 -0300

    added command and command struct and login handler

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/alifoo/blog-aggregator/internal/config"
 )
@@ -15,19 +17,76 @@ type command struct {
 	arguments []string
 }
 
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.handlers[cmd.name]
+	if !exists {
+		return fmt.Errorf("Command %s not found", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+
+func handlerLogin(s *state, cmd command) error {
+	if cmd.arguments == nil {
+		return errors.New("No arguments passed to the handler")
+	}
+
+	err := s.configPointer.SetUser(cmd.arguments[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("The user %v has been set.", cmd.arguments[0])
+
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(cfg)
 
-	cfg.SetUser("alifoo")
+	var s state
+	s.configPointer = &cfg
 
-	cfg, err = config.Read()
+
+	commands := commands{
+		handlers: make(map[string]func(*state, command) error),
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("No arguments passed to the command")
+		os.Exit(1)
+	}
+
+	cmd := command{
+		name: os.Args[1],
+		arguments: os.Args[2:],
+	}
+
+	commands.register("login", func(s *state, cmd command) error {
+		if len(cmd.arguments) == 0 {
+			return fmt.Errorf("a username is required for the login command")
+		}
+		username := cmd.arguments[0]
+		s.configPointer.SetUser(username)
+		fmt.Printf("Logged in as %s\n", username)
+    	return nil
+	})
+
+	err = commands.run(&s, cmd)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	fmt.Println(cfg)
 }
