c917f132d382cb0f5dab2f30f58819539785bd31
alifoo <alissonayres.trb@gmail.com>
2025-03-03 16:57:22 -0300

    state and command structs

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/alifoo/blog-aggregator/internal/config"
 )
 
+type state struct {
+	configPointer *config.Config
+}
+
+type command struct {
+	name string
+	arguments []string
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
